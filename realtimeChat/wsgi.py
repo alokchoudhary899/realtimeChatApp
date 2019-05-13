7f54e6718bc1c6ef5af613e16f3defec8fdaa108
alok <alokchoudhary899@gmail.com>
2019-05-13 17:07:19 +0530

    make it better

--- realtimeChat/wsgi.py
+++ realtimeChat/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtimeChat.realtimeChat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtimeChat.settings")
 application = get_wsgi_application()
